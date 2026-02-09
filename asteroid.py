8dfd839a38ff6be1b4a139db97e7f26419b422bc
Iammoocle <iammoocle@gmail.com>
2026-02-08 21:01:34 -0800

    shots now split asteroids into two smalller asteroids with new vectors

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH,ASTEROID_MIN_RADIUS
+from logger import log_state,log_event
+import random
 
 
 class Asteroid(CircleShape):
@@ -13,3 +15,18 @@ class Asteroid(CircleShape):
         
     def update(self,dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event('asteroid_split')
+            angle = random.uniform(20,50)
+            vector1 = pygame.math.Vector2.rotate(self.velocity,angle)
+            vector2 = pygame.math.Vector2.rotate(self.velocity,-angle)
+            self.radius -= ASTEROID_MIN_RADIUS
+            new_asteroid1 = Asteroid(self.position[0],self.position[1],self.radius)
+            new_asteroid2 = Asteroid(self.position[0],self.position[1],self.radius)
+            new_asteroid1.velocity = vector1 * 1.2
+            new_asteroid2.velocity = vector2 * 1.2
